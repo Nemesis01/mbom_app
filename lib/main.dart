8566e1f6165a6d6fb266db016ff99782e4b86828
Stéphane Nton <stephane.nton@gmail.com>
2020-05-17 00:43:16 -0400

    upgrading login screen

--- lib/main.dart
+++ lib/main.dart
@@ -28,94 +28,98 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // Try running your application with "flutter run". You'll see the
-          // application has a blue toolbar. Then, without quitting the app, try
-          // changing the primarySwatch below to Colors.green and then invoke
-          // "hot reload" (press "r" in the console where you ran "flutter run",
-          // or simply save your changes to "hot reload" in a Flutter IDE).
-          // Notice that the counter didn't reset back to zero; the application
-          // is not restarted.
-          primarySwatch: Colors.deepPurple,
-          primaryColor: Colors.white,
-          accentColor: Colors.deepPurple,
-          canvasColor: Colors.white,
-          fontFamily: 'Quicksand',
-          //typography: Typography.englishLike2018,
-          textTheme: TextTheme(
-            headline1: TextStyle(
-              fontSize: 96,
-              fontWeight: FontWeight.w300,
-              letterSpacing: -1.5,
-            ),
-            headline2: TextStyle(
-              fontSize: 60,
-              fontWeight: FontWeight.w300,
-              letterSpacing: -0.5,
-            ),
-            headline3: TextStyle(
-              fontSize: 48,
-              fontWeight: FontWeight.w400,
-              letterSpacing: 0.0,
-            ),
-            headline4: TextStyle(
-              fontSize: 34,
-              fontWeight: FontWeight.w400,
-              letterSpacing: 0.25,
-            ),
-            headline5: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.w400,
-              letterSpacing: 0.0,
-            ),
-            headline6: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.w500,
-              letterSpacing: 0.15,
-            ),
-            subtitle1: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.w400,
-              letterSpacing: 0.15,
-            ),
-             subtitle2: TextStyle(
-              fontSize: 14,
-              fontWeight: FontWeight.w500,
-              letterSpacing: 0.10,
-            ),
-            bodyText1: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.w400,
-              letterSpacing: 0.5,
-            ),
-            bodyText2: TextStyle(
-              fontSize: 14,
-              fontWeight: FontWeight.w400,
-              letterSpacing: 0.25,
-            ),
-             button: TextStyle(
-              fontSize: 14,
-              fontWeight: FontWeight.w500,
-              letterSpacing: 1.25,              
-            ),
-            caption: TextStyle(
-              fontSize: 12,
-              fontWeight: FontWeight.w400,
-              letterSpacing: 0.4,
-            ),
-            overline: TextStyle(
-              fontSize: 10,
-              fontWeight: FontWeight.w400,
-              letterSpacing: 1.5,
-            ),
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.deepPurple,
+        primaryColor: Colors.white,
+        accentColor: Colors.deepPurple,
+        canvasColor: Colors.white,
+        fontFamily: 'Quicksand',
+        /* appBarTheme: AppBarTheme(
+          
+          ),*/
+        appBarTheme: AppBarTheme(
+            brightness: Brightness.light,
+            textTheme: Theme.of(context).textTheme),
+        textTheme: TextTheme(
+          headline1: TextStyle(
+            fontSize: 96,
+            fontWeight: FontWeight.w300,
+            letterSpacing: -1.5,
           ),
-          buttonTheme: ButtonThemeData(
-            textTheme: ButtonTextTheme.normal
+          headline2: TextStyle(
+            fontSize: 60,
+            fontWeight: FontWeight.w300,
+            letterSpacing: -0.5,
           ),
-          buttonBarTheme: ButtonBarThemeData(),
-          iconTheme: IconThemeData(color: Colors.deepPurple),
-          cursorColor: Colors.black),
+          headline3: TextStyle(
+            fontSize: 48,
+            fontWeight: FontWeight.w400,
+            letterSpacing: 0.0,
+          ),
+          headline4: TextStyle(
+            fontSize: 34,
+            fontWeight: FontWeight.w400,
+            letterSpacing: 0.25,
+          ),
+          headline5: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.w400,
+            letterSpacing: 0.0,
+          ),
+          headline6: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.w500,
+            letterSpacing: 0.15,
+          ),
+          subtitle1: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w400,
+            letterSpacing: 0.15,
+          ),
+          subtitle2: TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.w500,
+            letterSpacing: 0.10,
+          ),
+          bodyText1: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w400,
+            letterSpacing: 0.5,
+          ),
+          bodyText2: TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.w400,
+            letterSpacing: 0.25,
+          ),
+          button: TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.w500,
+            letterSpacing: 1.25,
+          ),
+          caption: TextStyle(
+            fontSize: 12,
+            fontWeight: FontWeight.w400,
+            letterSpacing: 0.4,
+          ),
+          overline: TextStyle(
+            fontSize: 10,
+            fontWeight: FontWeight.w400,
+            letterSpacing: 1.5,
+          ),
+        ),
+        buttonTheme: ButtonThemeData(textTheme: ButtonTextTheme.primary),
+        buttonBarTheme: ButtonBarThemeData(),
+        iconTheme: IconThemeData(color: Colors.deepPurple),
+        //cursorColor: Colors.black,
+      ),
       //home: HomeScreen(),
       onGenerateRoute: _getRoute,
     );
